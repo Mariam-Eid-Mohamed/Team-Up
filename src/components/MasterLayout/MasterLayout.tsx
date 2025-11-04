63c0b48478316f2bfbc33c9a0c1a4932aaf5e847
mariameid <mariameid210@gmail.com>
2025-11-04 18:24:51 +0200

    Initial commit

---
+++ src/components/MasterLayout/MasterLayout.tsx
@@ -0,0 +1,5 @@
+const MasterLayout = () => {
+  return <div>MasterLayout</div>;
+};
+
+export default MasterLayout;
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,25 @@
+export interface LoginInputs {
+  email: string;
+  password: string;
+}
+
+export interface RegisterInputs {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+// export interface ResetInputs {
+//   otp: string;
+//   email: string;
+//   password: string;
+// }
+// export interface ChangePassInputs {
+//   password: string;
+//   password_new: string;
+// }
+// export interface ForgetInputs {
+//   email: string;
+// }
