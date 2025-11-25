426f6e73c13ddce1bd0cb9764809ebb6ab550083
mariameid <mariameid210@gmail.com>
2025-11-25 22:20:05 +0200

    added colors and fonts in index.css

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,11 @@
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <div>
+      <p className="text-primary">Primary text</p>
+      <div className="bg-soft-background p-4 rounded-xl">Soft BG</div>
+      <h2 className="text-accent">Accent text</h2>
+    </div>
+  );
 };
 
 export default AuthLayout;
