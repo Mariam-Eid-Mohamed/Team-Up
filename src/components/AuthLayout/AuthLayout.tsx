ba223be1b58c7a4442b5bf0df2681ba86adefe0f
hen1133 <helananemr692@gmail.com>
2025-11-26 14:08:20 +0200

    Your message

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,11 +1,29 @@
-const AuthLayout = () => {
+import { Outlet } from "react-router-dom";
+import AuthImg from "@/assets/images/authImg_resized.png";
+import logo from "@/assets/images/Logo.jpg";
+
+export default function AuthLayout() {
   return (
-    <div>
-      <p className="text-primary">Primary text</p>
-      <div className="bg-soft-background p-4 rounded-xl">Soft BG</div>
-      <h2 className="text-accent">Accent text</h2>
+    <div className="grid md:grid-cols-2 min-h-screen">
+      {/* LEFT IMAGE */}
+      <div className="order-2 md:order-1 hidden md:block">
+        <img 
+          src={AuthImg} 
+          alt="auth image"
+          className="w-full h-screen object-cover"
+        />
+      </div>
+      {/* RIGHT CONTENT */}
+      <div className="order-1 md:order-2 flex flex-col justify-center items-center p-6 overflow-auto">
+        {/* Logo */}
+        <img src={logo} alt="logo" className="mb-8 w-48" />
+
+        {/* Form container */}
+        <div className="w-full max-w-md">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
-};
+}
 
-export default AuthLayout;
