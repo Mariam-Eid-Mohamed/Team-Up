aac733c9bf19e6fdd99838da8412967af8ad7e7b
hen1133 <helananemr692@gmail.com>
2025-12-22 02:44:59 +0200

    edit logo

--- src/components/Navbar/InstructorNavbar.tsx
+++ src/components/Navbar/InstructorNavbar.tsx
@@ -1,18 +1,19 @@
 import { MessageCircle, Bell, Menu, ChevronDown } from "lucide-react";
-import logo from "@/assets/images/Logo.jpg";
+import logo from "@/assets/images/removebg-preview.png";
 
 const InstructorNavbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 h-14 flex items-center justify-between px-6 bg-[#83CDC4]">
       
       {/* Logo */}
-      <div className="flex items-center gap-2">
-        <img
-          src={logo}
-          alt="logo"
-          className="h-10 md:h-12 lg:h-16 w-auto"
-        />
-      </div>
+     <div className="flex items-center gap-2">
+  <img
+    src={logo}
+    alt="logo"
+    className="h-8 sm:h-9 md:h-10 lg:h-12 xl:h-12 w-auto max-h-12"
+  />
+</div>
+
 
       {/* Nav items */}
       <div className="flex items-center gap-6 text-black">
