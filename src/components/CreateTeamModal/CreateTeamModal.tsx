718cedb48020039f09442b2601a336799be8c47b
hen1133 <helananemr692@gmail.com>
2026-06-23 23:28:41 +0300

    fix create team modal issue

--- src/components/CreateTeamModal/CreateTeamModal.tsx
+++ src/components/CreateTeamModal/CreateTeamModal.tsx
@@ -67,7 +67,7 @@ const CreateTeamModal: React.FC<CreateTeamModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/50  p-4">
+    <div className="fixed inset-0 z-[100] min-h-screen backdrop-blur-sm flex items-center justify-center bg-black/50  p-4">
       <div className="bg-white w-full max-w-md rounded-2xl shadow-2xl p-6 relative animate-in fade-in zoom-in duration-200">
         <div className="flex justify-between items-center mb-8">
           <h2 className="text-xl font-bold text-gray-800">Create New Team</h2>
